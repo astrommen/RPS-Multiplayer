426147002235d1f7551c52426b6543ee39bd1102
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-02-03 16:22:02 -0500

    + firebase config

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    //Initializing firebase config
+    var firebaseConfig = {
+        apiKey: "AIzaSyASKw--Fcp-BPlJCexAKTkucxfYcKYe8zg",
+        authDomain: "student-project-749f4.firebaseapp.com",
+        databaseURL: "https://student-project-749f4.firebaseio.com",
+        projectId: "student-project-749f4",
+        storageBucket: "student-project-749f4.appspot.com",
+        messagingSenderId: "757173511560",
+        appId: "1:757173511560:web:70e8d536d60ac0a7d1463d"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+});
