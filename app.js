f61ff9d14bdbb634524baa40d23be49a37ab94aa
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-02-06 12:00:41 -0500

    + id playerBox; + name to dyn show; + full alert

--- app.js
+++ app.js
@@ -13,4 +13,47 @@ $(document).ready(function() {
 
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+
+    // Initializing var to ref the database
+    var database = firebase.database();
+    
+    // Initial Values
+    var player1 = "Waiting on Player 1...";
+    var player2 = "Waiting on Player 2...";
+    $("span").text("0");
+    var name = "No one here..."
+    var comment = "";
+
+    // Initializing var to grab display val
+    var user1 = $("#user1-display");
+    var user2 = $("#user2-display");
+    //setting input val to default vals
+    user1.text(player1);
+    user2.text(player2);
+    console.log(user1.text());//ftp only
+    
+    //when start btn click
+    $(document).on("click", "#start", function(){
+
+        //grabbing input val from userName
+        var Name = $("#userName").val();
+
+        //if both users have default val
+        if (user1.text() === player1 && user2.text() === player2){
+            
+            //then change user1 to name
+            user1.text(Name); 
+
+          // if user1 does not have default  
+        } else if (user1.text() !== player1 && user2.text() === player2){
+
+            //then change user2 to name
+            user2.text(Name);
+
+        } else {
+            alert("Game is full, please try again later!");
+        }
+    });
+
+
 });
